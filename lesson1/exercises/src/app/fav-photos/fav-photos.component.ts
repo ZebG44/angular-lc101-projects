c78564f2ec23675acb629897ae54a023edd0293c
Zeb <zebgosik@yahoo.com>
2021-08-14 21:54:17 -0400

    lesson 1 part 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Favorite Photos';
+  image1 = 'https://spaceplace.nasa.gov/gallery-sun/en/solar-flare.en.jpg';
+  image2 = 'http://webmeup.com/upload/blog/lead-image-105.png';
+  image3 = 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg';
 
   constructor() { }
 
